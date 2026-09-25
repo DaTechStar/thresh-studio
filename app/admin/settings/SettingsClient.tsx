c2aafc655aeff4a7f14ec896d23e974be8b917b2
legend4tech <ajuludennis14@gmail.com>
2026-09-25 16:55:28 +0100

    fix: move use client directive to top of SettingsClient

--- app/admin/settings/SettingsClient.tsx
+++ app/admin/settings/SettingsClient.tsx
@@ -1,5 +1,6 @@
+"use client"
+
 import { SiteSettingsValues } from "@/lib/schemas"
-;("use client")
 
 import React, { useState, useEffect } from "react"
 import { useRouter, useSearchParams, usePathname } from "next/navigation"
